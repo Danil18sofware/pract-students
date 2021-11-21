d9ea7991a0fc016e39c4cb493de850ceb77d81ac
Danil18sofware <81704383+Danil18sofware@users.noreply.github.com>
2021-11-21 15:23:48 +0200

    first

---
+++ src/components/StudentsInfo.vue
@@ -0,0 +1,38 @@
+<template>
+   <div class="main-stud">
+        <div>
+            <img :src="student.photo">
+        </div>
+        <div class="stud-about">
+            <p>ФИО: {{student.name}}</p>
+            <p>Группа: {{student.group}}</p>
+            <p>Оценка: {{student.mark}}</p>
+        </div>        
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+    props:{
+        id:'',
+    },
+    data: function(){
+        return{
+            student:{},
+        };
+    },
+    mounted: function(){
+        Vue.axios.get("http://46.101.212.195:3000/students/"+this.id).then((response)=>{
+            console.log(response.data)
+            this.student=response.data;
+        })
+    },
+    computed:{
+            getCount(){
+                return this.$store.getters['getCount']
+            }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import Students from './components/Students.vue'
+import StudentsInfo from './components/StudentsInfo.vue'
+import App from './components/App.vue'
+import Login from './components/Login.vue'
+import store from './store.js' 
+
+const routes = [
+    {path: '/', component: Students, meta:{requiresAuth:true}},
+    {path: '/students-info/:id', component: StudentsInfo, props: true, meta:{requiresAuth:true}},
+    {path: '/login', component: Login}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+router.beforeEach((to, from, next)=>{
+    if (to.matched.some(record => record.meta.requiresAuth)){
+        if (store.getters.getUser === null){
+            next({
+                path:'/login',
+                query: {redirect: to.fullPath}
+            })
+        } else{
+            next()
+        } 
+    } else {
+        next()
+    }
+})
+    Vue.use( VueAxios, axios)
+    Vue.use(VueRouter)
+
+new Vue({
+   render: h => h(App),
+   el: '#app',
+   store,
+   router
+})
